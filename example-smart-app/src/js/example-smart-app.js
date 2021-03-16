619f2524fe850fe2cb29e3bcddace1f78728939a
jtslimited <77407613+jtslimited@users.noreply.github.com>
2021-03-16 16:29:11 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -31,7 +31,7 @@
           var fname = '';
           var lname = '';
           var accountscount = '0';
-          var smartobject = JSON.stringify(smart);
+          var smartobject = JSON.stringify(smart.patient);
           
           if(smart.hasOwnProperty('account')) {
             var accountscount = '82';
